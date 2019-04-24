d5c89ec5488fc17e3d29d33d6cfb3aff68870887
nikol kat <nikolk2@gmail.com>
2019-04-24 12:24:42 +0300

    finished the VIEW for main window, flight controls(manual and auto) and flight board. settings window NOT done yet.

--- FlightSimulator/Views/FlightControls.xaml.cs
+++ FlightSimulator/Views/FlightControls.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulator
+namespace FlightSimulator.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FlightControls.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FlightControls : UserControl
     {
-        public MainWindow()
+        public FlightControls()
         {
             InitializeComponent();
         }
-
     }
 }
